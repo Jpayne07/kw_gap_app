c9cd5d4d38bbf80b3f016f2d40c4b48e17ae6304
Jacob <jacobpayne007@gmail.com>
2024-12-15 23:25:33 -0600

    done

---
+++ server/migrations/versions/95522d55e2ee_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 95522d55e2ee
+Revises: 
+Create Date: 2024-12-13 21:49:09.118154
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '95522d55e2ee'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
