0a2ae1fefaecbbd428c324c11d8552dd7a80f940
Jacob <jacobpayne007@gmail.com>
2024-12-30 23:29:50 -0600

    more changes

--- server/config.py
+++ server/config.py
@@ -11,7 +11,11 @@ from flask_bcrypt import Bcrypt
 
 # Local imports
 # Instantiate app, set attributes
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+    static_folder='../client/build',
+    template_folder='../client/build'
+)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
